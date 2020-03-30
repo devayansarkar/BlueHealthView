33f79a93f2498718f52a33996b2093a6676153ec
Devayan Sarkar <devayansarkar@gmail.com>
2020-03-30 04:36:04 +0200

    Adding Material Design Dialog because Semantic-UI was taking a lot of time

--- src/app/components/appointments/appointments.component.ts
+++ src/app/components/appointments/appointments.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Inject } from '@angular/core';
+import { FeedbackComponent } from './feedback/feedback.component';
+import {MatDialog,MatDialogRef} from '@angular/material/dialog';
+import { PrescriptionComponent } from './prescription/prescription.component';
 @Component({
   selector: 'app-appointments',
   templateUrl: './appointments.component.html',
@@ -9,7 +11,8 @@ export class AppointmentsComponent implements OnInit {
   isAppointmentCreated = false;
   isFormOpen = false;
   isSearchOpen = false;
-  constructor() { }
+  name = "Smith";
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {
   }
@@ -30,5 +33,22 @@ export class AppointmentsComponent implements OnInit {
     this.isAppointmentCreated = false;
     this.isFormOpen = false;
     this.isSearchOpen = true;
+  }
+
+  openFeedback(): void {
+    const dialogRef = this.dialog.open(FeedbackComponent, {
+      width: '50%'});
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+  openPrescription(): void {
+    const dialogRef = this.dialog.open(PrescriptionComponent,{
+      width: '50%'});
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
   }
 }
